0ea752ab63e3c75e4f10e18dcb128e7552bbc49c
90sPixelDev <fontlien@gmail.com>
2022-05-01 10:45:25 -0400

    Added a lot better design and layout (for browse page) Added the main fetch request

---
+++ scripts/browse.js
@@ -0,0 +1,89 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+		'X-RapidAPI-Key': '3a60d8be74msh2c52c4cf188b0cep1cfe1fjsn43e75d3457a2',
+	},
+};
+
+const gameItem = document.querySelectorAll('.game-item');
+const nextPage = document.querySelector('.next-page');
+const prevPage = document.querySelector('.prev-page');
+let page = 1;
+
+const loadGameList = (data) => {
+	let lastIndex;
+	let startIndex;
+	if (page === 1) {
+		startIndex = 0;
+		lastIndex = 15;
+	}
+	switch (page) {
+		case 2:
+			lastIndex = 15 * page;
+			break;
+		case 3:
+			lastIndex = 15 * page;
+			break;
+		case 4:
+			lastIndex = 15 * page;
+			break;
+		case 5:
+			lastIndex = 15 * page;
+			break;
+	}
+	let titleArr = [];
+	for (let i = startIndex; i < lastIndex; i++) {
+		let title = data[i].title;
+		let imgURL = data[i].thumbnail;
+		titleArr.push(title);
+		gameItem[i].innerText = titleArr[i];
+		createGameImg(gameItem[i], imgURL);
+		titleSize(gameItem[i], titleArr[i]);
+	}
+};
+
+const createGameImg = (el, imgURL) => {
+	const img = document.createElement('img');
+	img.src = imgURL;
+	el.append(img);
+};
+
+const titleSize = (gameItem, gameTitle) => {
+	let textSize = window.getComputedStyle(gameItem, null).getPropertyValue('font-size');
+	if (gameTitle.length > 15) gameItem.style.fontSize = '15px';
+	else if (gameTitle.length > 20) gameItem.style.fontSize = '10px';
+};
+
+const checkStatus = (response) => {
+	if (!response.ok) throw new Error('Something went wrong');
+	console.log('Status: OK');
+	return response.json();
+};
+
+fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+	.then(checkStatus)
+	.then((data) => {
+		console.log(data);
+		return data;
+	})
+	.then(loadGameList)
+	.catch((err) => console.error(err));
+
+nextPage.addEventListener('click', () => {
+	console.log('Pressed the next page!');
+	page++;
+	console.log('Page: ' + page);
+	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+		.then(checkStatus)
+		.then(loadGameList);
+});
+prevPage.addEventListener('click', () => {
+	if (page === 1) return;
+	console.log('Pressed the previous page!');
+	page--;
+	console.log('Page: ' + page);
+	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+		.then(checkStatus)
+		.then(loadGameList);
+});
