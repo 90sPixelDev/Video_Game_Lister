48d881340d30b430738677aecbeabe44ee279937
90sPixelDev <fontlien@gmail.com>
2022-05-09 04:56:06 -0400

    changed pathname bc netlify does not like capital letters for deployment in submodules apparently

--- scripts/video_game_lister.js
+++ scripts/video_game_lister.js
@@ -171,9 +171,8 @@ const titleSize = (gameItem, gameTitle) => {
 
 // LOADING INITIAL DATA
 document.addEventListener('DOMContentLoaded', async () => {
-	if (window.location.pathname === '/projects/Video_Game_Lister/browse.html') {
-		// if (window.location.pathname === '/projects/video_game_lister/browse.html') {
-
+	// if (window.location.pathname === '/projects/Video_Game_Lister/browse.html') {
+	if (window.location.pathname === '/projects/video_game_lister/browse.html') {
 		console.log('Correct Path!');
 		await loadGameList();
 
